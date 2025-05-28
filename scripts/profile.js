d556c826fd0b6af89e629f9631f09c1c087faac5
Sally <salomeytackie555@gmail.com>
2025-05-28 15:33:03 +0000

    created registered and login form

---
+++ scripts/profile.js
@@ -0,0 +1,22 @@
+// Get user profile
+fetch('https://api.escuelajs.co/api/v1/auth/profile', {
+    headers:{
+        'Authorization': `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
+    }
+})
+.then(response => response.json())
+.then(data => {
+    // Access user info div from DOM
+    const userInfo = document.getElementById('user-info');
+    // Create and append user name to user info div
+    const userName = document.createElement('h1');
+    userName.innerText = data.name;
+    userName.classList.add('text-3xl', 'font-bold', 'underline');
+    userInfo.appendChild(userName);
+    // Create and append user avatar to user info div
+    const userImg = document.createElement('img');
+    userImg.src = data.avatar;
+    userImg.classList.add('w-50');
+    userInfo.appendChild(userImg);
+})
+.catch(error => console.log(error));
